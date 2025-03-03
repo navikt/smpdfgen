80bc1743ad43b93c9901968585ad8d47b8ff5ab7
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2025-03-03 14:58:27 +0100

    chore: bumped pdfgen to 2.0.69

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ghcr.io/navikt/pdfgen:2.0.68
+FROM ghcr.io/navikt/pdfgen:2.0.69
 
 COPY templates /app/templates
 COPY fonts /app/fonts
--- run_development.sh
+++ run_development.sh
@@ -2,7 +2,7 @@
 
 CURRENT_PATH="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
 
-docker pull ghcr.io/navikt/pdfgen:2.0.68
+docker pull ghcr.io/navikt/pdfgen:2.0.69
 docker run \
         -v $CURRENT_PATH/templates:/app/templates \
         -v $CURRENT_PATH/fonts:/app/fonts \
@@ -14,4 +14,4 @@ docker run \
         -e DEV_MODE=true \
         -it \
         --rm \
-        ghcr.io/navikt/pdfgen:2.0.68
+        ghcr.io/navikt/pdfgen:2.0.69
